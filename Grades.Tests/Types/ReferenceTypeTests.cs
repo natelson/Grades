ac1eaa2f77abd06672a5ebcb432bd8e72bbfe6b4
natelson <natelson.nascimento@gmail.com>
2017-02-28 14:43:16 -0300

    The first commit

---
+++ Grades.Tests/Types/ReferenceTypeTests.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades.Tests.Types
+{
+    [TestClass]
+    public class ReferenceTypeTests
+    {
+
+        [TestMethod]
+        public void UppercaseString()
+        {
+            string name = "natelson";
+            name = name.ToUpper();
+
+            Assert.AreEqual("NATELSON", name);
+        }
+
+        [TestMethod]
+        public void AddDaysToDateTime()
+        {
+            DateTime date = new DateTime(2016, 1, 1);
+            date = date.AddDays(1);
+
+            Assert.AreEqual(2, date.Day);
+        }
+
+        [TestMethod]
+        public void ValueTypePassByValue()
+        {
+            int x = 46;
+            IncrementNumber(x);
+
+            Assert.AreEqual(46, x);
+        }
+
+
+        private void IncrementNumber(int x)
+        {
+            x += 1;
+        }
+
+        [TestMethod]
+        public void ReferenceTypesPassByValue()
+        {
+            GradeBook book1 = new GradeBook();
+            GradeBook book2 = book1;
+
+            GiveBookAName(book2);
+
+            Assert.AreEqual("A GradeBook", book1.Name);
+        }
+
+        private void GiveBookAName(GradeBook book)
+        {
+            book.Name = "A GradeBook";
+        }
+
+        [TestMethod]
+        public void StringComparisons()
+        {
+            string name1 = "Natelson";
+            string name2 = "natelson";
+
+            bool result = String.Equals(name1, name2, StringComparison.InvariantCultureIgnoreCase);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void IntVariablesHoldValue()
+        {
+            int x1 = 100;
+            int x2 = x1;
+            x1 = 4;
+
+            Assert.AreNotEqual(x1, x2);
+        }
+
+        
+
+           
+        [TestMethod]
+        public void GradeBookVariableHoldAReference()
+        {
+            GradeBook g1 = new GradeBook();
+            GradeBook g2 = g1;
+            g1.Name = "Natelson's grade book";
+            Assert.AreEqual(g1.Name, g2.Name);
+        }
+    }
+}
